c38b6bb4c4996aa92c60bfffd1f88a68ff4a5234
elias <elias.boegel@yahoo.de>
2024-02-19 10:29:06 +0100

    Add normalization option to coefficient plot, add save feature to plotter

--- src/plotter.py
+++ src/plotter.py
@@ -1,18 +1,25 @@
 import numpy as np
 import scipy.special as ssp
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from pathlib import Path
 
 class Plotter:
-    def __init__(self, resolution=500, velocity_bounds=[-3,3]):
-        plt.ion()
-        self.resolution = resolution
+    def __init__(self, save_directory=None, v_resolution=500, velocity_bounds=[-3,3], show=False):
+        if show: plt.ion()
+        self.v_resolution = v_resolution
         self.velocity_bounds = velocity_bounds
         self.im = plt.imshow(np.zeros([1,1]))
-        self.v = np.linspace(velocity_bounds[0], velocity_bounds[1], resolution)
+        self.v = np.linspace(velocity_bounds[0], velocity_bounds[1], v_resolution)
+        self.show = show
+        self.save_directory = Path(save_directory).mkdir(parents=True, exist_ok=True) # Create directory if it does not yet exist
+        print(self.save_directory)
 
-    def plot1V(self, sys, species_ids, delay=1e-1):
+
+
+    def plot1V(self, sys, species_ids, t=None, E=None, delay=1e-1):
         plt.clf()
-        vals = np.zeros([sys.domain['N'], self.resolution])
+        vals = np.zeros([sys.domain['N'], self.v_resolution])
 
         for s in species_ids:
             if (sys.species[s].num_modes>1).sum() != 1: # Counts how many velocity axes have more than 1 mode, if multiple have more than 1 mode, then this is not a 1V case and can´t be drawn as 2D plot
@@ -20,8 +27,7 @@ class Plotter:
 
             velocity_axis = np.argmax(sys.species[s].num_modes>1) # Find which velocity axis is the relevant one (i.e. the only one with >1 modes)
             n = np.arange(sys.species[s].num_modes[velocity_axis])
-
-            xi = (self.v - sys.species[s].u[velocity_axis]) / sys.species[s].alpha[velocity_axis]
+            xi = (self.v - sys.species[s].shift[velocity_axis]) / sys.species[s].scale[velocity_axis]
 
             C_s = sys.all_C(s)            
             for x in range(sys.domain['N']):
@@ -32,19 +38,29 @@ class Plotter:
                 elif (velocity_axis==2):
                     C_x = C_s[0,0,:,x]
                 
-                # if x==0 and s==0: print(C_s[:,:,:,x])
-                # exit()
-                # print(np.power(2, n, dtype=float))
-                # print(ssp.factorial(n))
-                # print((np.power(2, n, dtype=float) * ssp.factorial(n)))
-                # exit()
-                
-                C_x_HF = (np.power(2, n, dtype=float) * ssp.factorial(n) * np.pi**0.5)**-0.5 * C_x # Include (2^n * n! * sqrt(pi))^-0.5 term in coefficients
-                # if x==0 and s==0: print(C_x_HF)
+                C_x_HF = (np.power(2, n, dtype=float) * ssp.factorial(n) * np.pi)**-0.5 * C_x # Include (2^n * n! * sqrt(pi))^-0.5 term in coefficients
                 
                 vals[x,:] += np.polynomial.hermite.hermval(xi, C_x_HF) * np.exp(-xi**2) # Evaluate Hermite function series
         
-        plt.imshow(vals.T, origin='lower', extent=[sys.domain['L'], sys.domain['R'], self.velocity_bounds[0], self.velocity_bounds[1]])
+        plt.imshow(vals.T, origin='lower', extent=[sys.domain['L'], sys.domain['R'], self.velocity_bounds[0], self.velocity_bounds[1]], cmap="jet")
+        plt.gca().set_ylabel(r"v")
+        if t is not None: plt.gca().set_title(f"t = {t:.3f}")
         plt.colorbar()
+
+        if self.save_directory is not None: plt.savefig(f"{save_directory}/{t_idx}_vdf.png")
                         
-        plt.pause(delay)
+        if self.show: plt.pause(delay)
+
+    def plot_coefficients(self, sys, s, modes, normalize=False, t=None, delay=1e-1):
+        plt.clf()
+        for i,j,k in modes:
+            vals = sys.C(s,i,j,k)
+            vals_max, vals_min = vals.max(), vals.min()
+            if normalize and vals_max != 0 and vals_min != 0 and vals_max!=vals_max:
+                vals = (vals - vals_min) / (vals_max - vals_min)
+            plt.plot(vals, label=f"$C_{{{i},{j},{k}}}$, max={vals_max}")
+        plt.gca().set_xlabel(r"z")
+        plt.legend()
+        if t is not None: plt.gca().set_title(f"t = {t:.3f}")
+        if self.save_directory is not None: plt.savefig(f"{save_directory}/{t_idx}_coefficients.png")
+        if self.show: plt.pause(delay)
