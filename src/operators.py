58b13a7c548e09bb161155ba9748711d5f5d28a1
elias <elias.boegel@yahoo.de>
2024-03-19 17:46:03 +0100

    Move negative sign of LHS into the operators so that all operators can be composed with +=

--- src/operators.py
+++ src/operators.py
@@ -34,7 +34,7 @@ class Advection:
         terms += sys.C(s,i,j,k-2) * np.sqrt(k*(k-1))*( u[2]*da[2]/a[2] + du[2] )
         terms += sys.C(s,i,j,k-3) * np.sqrt(k*(k-1)*(k-2)/2)*da[2]
 
-        return terms
+        return - terms
 
     @staticmethod
     def cylindrical(sys, s, i, j, k):
@@ -44,7 +44,7 @@ class Acceleration():
     @staticmethod
     def electric(sys, s, i, j, k, E): # Only one component as model is 1D and therefore Ey=0, Ez=0
         terms = - E * sys.species[s].q/sys.species[s].m * np.sqrt(2*k) / sys.species[s].scale[2] * sys.C(s, i, j, k-1)
-        return terms
+        return - terms
 
     @staticmethod
     def magnetic(sys, s, i, j, k, B):
@@ -63,7 +63,7 @@ class Acceleration():
         terms += - sys.species[s].q/sys.species[s].m * B[1] * np.sqrt(2*k)/a[2] * (a[0]*np.sqrt((i+1)/2)*sys.C(s,i+1,j,k-1) + u[0]*sys.C(s,i,j,k-1) + a[0]*np.sqrt(i/2)*sys.C(s,i-1,j,k-1))
         terms +=   sys.species[s].q/sys.species[s].m * B[0] * np.sqrt(2*k)/a[2] * (a[1]*np.sqrt((j+1)/2)*sys.C(s,i,j+1,k-1) + u[1]*sys.C(s,i,j,k-1) + a[1]*np.sqrt(j/2)*sys.C(s,i,j-1,k-1))
         
-        return terms        
+        return - terms        
 
     @staticmethod
     def centripetal(sys, s, i, j, k):
@@ -78,7 +78,7 @@ class Acceleration():
         terms += sys.C(s,i-1,j+1,k) * u[1]*a[1] * np.sqrt((j+1)/2)
         terms += sys.C(s,i-1,j-2,k) * a[1]**2/2 * np.sqrt(j*(j-1))
         terms *= - 1/r*np.sqrt(2*i)/a[0]
-        return terms
+        return - terms
 
     @staticmethod
     def coriolis(sys, s, i, j, k):
@@ -96,27 +96,27 @@ class Acceleration():
         terms += sys.C(s,i+1,j-2,k) * np.sqrt(j*(j-1)*(i+1)/2) * a[0]
         terms += sys.C(s,i,j-2,k) * np.sqrt(j*(j-1)) * u[0]
         terms += sys.C(s,i-1,j-2,k) * np.sqrt(i*j*(j-1)/2) * a[0]
-        terms *= 2/r
-        return terms
+        terms *= 1/r
+        return - terms
 
 class KineticEquation(): # Implementation of kinetic equation in form dC/dt = terms
     @staticmethod
-    def cartesian(sys, s, i, j, k, E=0, B=0):
+    def cartesian(sys, s, i, j, k, E, B):
         terms = 0
-        terms -= Advection.cartesian(sys, s, i, j, k)
-        terms -= Acceleration.electric(sys, s, i, j, k, E)
-        terms -= Acceleration.magnetic(sys, s, i, j, k, B)
+        terms += Advection.cartesian(sys, s, i, j, k)
+        terms += Acceleration.electric(sys, s, i, j, k, E)
+        terms += Acceleration.magnetic(sys, s, i, j, k, B)
         terms += Collision.lenard_bernstein(sys, s, i, j, k)
         return terms
 
     @staticmethod
-    def cylindrical(sys, s, i, j, k, E=0, B=0):
+    def cylindrical(sys, s, i, j, k, E, B):
         terms = 0
-        terms -= Advection.cylindrical(sys, s, i, j, k)
-        terms -= Acceleration.electric(sys, s, i, j, k, E)
-        terms -= Acceleration.magnetic(sys, s, i, j, k, B)
-        terms -= Acceleration.centripetal(sys, s, i, j, k)
-        terms -= Acceleration.coriolis(sys, s, i, j, k)
+        terms += Advection.cylindrical(sys, s, i, j, k)
+        terms += Acceleration.electric(sys, s, i, j, k, E)
+        terms += Acceleration.magnetic(sys, s, i, j, k, B)
+        terms += Acceleration.centripetal(sys, s, i, j, k)
+        terms += Acceleration.coriolis(sys, s, i, j, k)
         terms += Collision.lenard_bernstein(sys, s, i, j, k)
         return terms
 
@@ -140,3 +140,8 @@ class Time(): # Implicit time discretization operators in format f=0
         K_curr = K(sys_curr, s, i, j, k, E, B)
         K_next = K(sys_next, s, i, j, k, E, B)
         return C_next - C_curr - 0.5 * (K_curr + K_next) * dt
+
+class Preconditioner():
+    @staticmethod
+    def linear_ilu(sys, E, B):
+        return None
